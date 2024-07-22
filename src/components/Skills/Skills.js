c236eb833b22d4b6ff228586d7b0860b5b7d9f37
Mrityunjay <mjguptacse@gmail.com>
2024-07-22 21:35:52 +0530

    updated skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -21,7 +21,7 @@ function Skills() {
         <h2 style={{ color: theme.primary }}>Skills</h2>
       </div>
       <div className="skillsContainer">
-        <div className="skill--scroll mrityunjay">
+        <div className="skill--scroll">
           <Marquee
             gradient={false}
             speed={80}
